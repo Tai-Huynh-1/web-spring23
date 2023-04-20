6057d5b578f31c1d3bffa9c08ec041a35cf8489e
Tai-Huynh-1 <93554992+Tai-Huynh-1@users.noreply.github.com>
2023-04-20 15:35:24 -0700

    copy by reference & compare by reference

--- class/js/compare.js
+++ class/js/compare.js
@@ -18,11 +18,28 @@ a = 10; // reassign a = 10
 // console.log("line 18", a === b); // 10 === 5 => false
 // console.log("line 19", b === c); // 5 === 5 => true
 
-let arrA = [a]; // [10]
-let arrB = arrA; // [10]
-let arrC = [a]; // [10]
+// ['jack', 'ana', 'joe', 5, [], true, null, undefined, {}]
 
-console.log("arrA === arrB", arrA === arrB); // [10] === [10] => true
-console.log("arrA === arrC", arrA === arrC); // [10] === [10] => true
+let arrA = [a]; // [10] - direct assignment to [] - arrays are stored in a memory address '000111'
+let arrB = arrA; // [10] - COPY (by value or by reference?) - '000111'
+let arrC = [a]; // [10] - direct assignment to [] - '111000'
+
+// console.log("arrA === arrB", arrA === arrB); // [10] === [10] => true, but actually: '000111' === '000111' => true
+// console.log("arrA === arrC", arrA === arrC); // [10] === [10] => true, '000111' === '111000' => false
+// console.log("arrA", arrA);
+// console.log("arrC", arrC);
 
 // make change to array A, then compare with array B again
+console.log("==== MAKING MODIFICATION TO ARRA =====");
+arrA[1] = 100;
+console.log("arrA", arrA); // [10, 100]
+console.log("arrB", arrB); // [10, 100]
+console.log("arrA === arrB", arrA === arrB); // true, they are the same array in memory
+console.log("arrA === [10, 100]", arrA === [10, 100]); // false
+
+console.log("==== NOW MAKING MODIFICATION TO ARRB =====");
+
+arrB[1] = 555; // NOT THE SAME as arrB = 555 / underneath it's doing arrA[1] = 555
+console.log("arrA", arrA); // [10, 555]
+console.log("arrB", arrB); // [10, 555]
+console.log("arrA === arrB", arrA === arrB); // true, true: same memory address, true: array a & b has same memory address
