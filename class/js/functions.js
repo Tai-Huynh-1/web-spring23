cc264315b82231d41bf62b160bc55df126f2ac5f
Tai-Huynh-1 <93554992+Tai-Huynh-1@users.noreply.github.com>
2023-04-28 15:26:57 -0700

    call stack, loops and array methods

--- class/js/functions.js
+++ class/js/functions.js
@@ -51,16 +51,36 @@ function blockFunc(boolean) {
 	if (boolean) {
 		answer = 1;
 		console.log(answer);
+		printMyAge();
 	} else {
 		let answer = 2;
 		answer = 1;
 		console.log(answer); //
+		printMyAge();
 	}
 
 	return answer;
 }
 
-console.log(blockFunc(false)); //
+// const result = blockFunc(false); //
+// console.log(result);
+
+// execution flow & call stack in JS
+// stack vs queue
+// queue: first in first out
+// stack: last in first out
+
+// ___________
+// function.js
+// function.js > blockFunc
+// function.js > blockFunc > console.log(answer)
+// function.js > blockFunc
+// function.js > blockFunc > printMyAge()
+// function.js > blockFunc
+// function.js > console.log(result)
+// function.js
+
+// ___________
 
 // == vs ===
 // console.log(10 === "10"); // false, false => false (type not same)
@@ -73,3 +93,56 @@ console.log(blockFunc(false)); //
 // console.log(10 == "ten"); // false, false => false
 // console.log("ten" == "10"); // false, false => false (scalar value is not same)
 // console.log("ten" == "010"); // false, true => false (scalar value is not same)
+
+// Given an array of numbers, [56, 84, 56, 800, 47, 6, 1, 0, 555, 647], write a function that returns the largest value from the input array.
+const input = [56, 84, 56, 800, 47, 6, 1, 0, 555, 647];
+function findLargestNumber(array) {
+	let largest = array[0];
+	// let i = 1; // index iterator
+	const lastIndex = array.length - 1;
+
+	for (let i = 1; i < lastIndex; i++) {
+		const currentNumber = array[i];
+		if (currentNumber > largest) {
+			largest = currentNumber;
+		}
+	}
+
+	// while (i < lastIndex) {
+	// 	const currentNumber = array[i];
+	// 	if (currentNumber > largest) {
+	// 		largest = currentNumber;
+	// 	}
+	// 	// make sure to increment i so the while loop condition eventually becomes false
+	// 	i++;
+	// }
+
+	return largest;
+}
+
+// console.log(findLargestNumber(input));
+
+// iterate over array with an array method
+function findLargestNumberUsingForEach(array) {
+	let largest = array[0];
+
+	array.forEach(function (currentNumber) {
+		if (currentNumber > largest) {
+			largest = currentNumber;
+		}
+	});
+
+	return largest;
+}
+
+// console.log(findLargestNumberUsingForEach(input));
+
+function customForEach(cb, array) {
+	for (let i = 0; i < array.length; i++) {
+		cb();
+	}
+}
+
+// demonstrate iterating through every item vs end early with for loop vs forEach
+
+//
